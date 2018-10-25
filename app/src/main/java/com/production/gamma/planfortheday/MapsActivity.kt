3aae50d22c08d85879205816a27df04c1aea6131
Valentin Rudloff <gamma_production.support@live.fr>
2018-10-25 23:44:40 +0200

    fix merge

--- app/src/main/java/com/production/gamma/planfortheday/MapsActivity.kt
+++ app/src/main/java/com/production/gamma/planfortheday/MapsActivity.kt
@@ -4,18 +4,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 import java.util.*
-import android.graphics.Typeface
 import android.R.attr.y
 import android.app.Activity
 import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.graphics.*
 import java.lang.reflect.Type
 import android.graphics.drawable.Drawable
 import android.location.Location
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.ActivityCompat
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
 
@@ -52,9 +64,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
     private fun initViews(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -67,13 +76,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         button.setOnClickListener(this)
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
-
-
+    }
+    private fun initAnimation(){
         openAddPlanItem = AnimationUtils.loadAnimation( this, R.anim.open_add_button)
         closeAddPlanItem = AnimationUtils.loadAnimation( this, R.anim.close_add_button)
         openRotateAddPlanButton = AnimationUtils.loadAnimation( this, R.anim.open_rotation)
         closeRotateAddPlanButton = AnimationUtils.loadAnimation( this, R.anim.close_rotation)
-
 
         openRotateAddPlanButton.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
@@ -120,6 +128,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         })
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        initVar()
+        initViews()
+        initAnimation()
+
+    }
+
     override fun onClick(v: View?) {
         when(v)
         {
@@ -155,7 +173,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         button1.startAnimation(openAddPlanItem)
         button2.startAnimation(openAddPlanItem)
     }
-
     private fun closeMenu(enableAnimation: Boolean){
         if(enableAnimation)
             button.startAnimation(closeRotateAddPlanButton)
@@ -167,15 +184,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         button2.startAnimation(closeAddPlanItem)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -186,13 +194,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
             zoomPlan()
             addToPlan(coordsPointed)
         }
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        mMap.setOnMapClickListener { p0 -> Log.d("Map", p0.toString())
-            mMap.addMarker(MarkerOptions().position(p0).title("marker")) }
     }
 
     private fun zoomToLocation() {
